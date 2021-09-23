57ac2d1c20dca01ab6b8a57e8645413f5bb4aa25
PanagiotisDaBest <90244906+PanagiotisDaBest@users.noreply.github.com>
2021-09-23 14:15:34 +0200

    blev klar med extra uppgift

--- Matrix/Program.cs
+++ Matrix/Program.cs
@@ -80,7 +80,7 @@ namespace Matrix
             radius = 3.14f;
             a00 = (float)Math.Cos(radius);
             a01 = (float)Math.Sin(radius);
-            a10 = (float)Math.Sin(radius);
+            a10 = (float)-Math.Sin(radius);
             a11 = (float)Math.Cos(radius);
 
             MatrixChecker.CheckRotationMatrix(radius, a00, a01, a10, a11);
@@ -105,7 +105,30 @@ namespace Matrix
             MatrixChecker.CheckMultiplication(a00, a01, a10, a11, b00, b01, b10, b11, c00, c01, c10, c11);
             Draw2x2Matrix(c00, c01, c10, c11);
 
+
+            float användaren = float.Parse(Console.ReadLine());
+            a00 = (float) Math.Cos(användaren);
+            a01 = (float) Math.Sin(användaren);
+            a10 = (float) -Math.Sin(användaren);
+            a11 = (float) Math.Cos(användaren);
+            b00 = (float)Math.Cos(användaren);
+            b01 = (float)Math.Sin(användaren);
+            b10 = (float)-Math.Sin(användaren);
+            b11 = (float)Math.Cos(användaren);
+
+            c00 = a00 * b00 + a01 * b10;
+            c01 = a00 * b01 + a01 * b11;
+            c10 = a10 * b00 + a11 * b10;
+            c11 = a10 * b01 + a11 * b11;
+
+            MatrixChecker.CheckMultiplication(a00, a01, a10, a11, b00, b01, b10, b11, c00, c01, c10, c11);
+            Draw2x2Matrix(c00, c01, c10, c11);
+
             Console.ReadKey();
+
+
         }
+
+
     }
 }
